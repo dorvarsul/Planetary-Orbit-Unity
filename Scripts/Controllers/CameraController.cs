2ad38772fd49b729fd469ced4d4429a1ae21192f
Dor Varsulker <86845455+dorvarsul@users.noreply.github.com>
2022-10-21 15:32:14 +0300

    updated CameraController
    better camera followup suited for spaceships and 3D space

--- Scripts/Controllers/CameraController.cs
+++ Scripts/Controllers/CameraController.cs
@@ -5,16 +5,55 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.3f;
-    public Vector3 offset;
-    private Vector3 velocity = Vector3.zero;
+    public float dis = 10f;
+    public float height = 5f;
 
-    void FixedUpdate()
+    public float heightDamping = 2.0f;
+    public float rotationDamping = 3.0f;
+
+    void LateUpdate()
     {
-        Vector3 desiredPos = target.position + offset;
-        Vector3 smoothedPos = Vector3.SmoothDamp(transform.position, desiredPos, ref velocity, smoothSpeed);
-        transform.position = smoothedPos;
+        if (!target) { return; }
 
-        transform.LookAt(target);
+        Vector3 followPos = new Vector3(0.0f, height, -dis);
+        Quaternion lookRotation = Quaternion.identity;
+
+        lookRotation.eulerAngles = new Vector3(30.0f,0.0f,0.0f);
+
+        Matrix4x4 m1 = Matrix4x4.TRS(target.position, target.rotation, Vector3.one);
+        Matrix4x4 m2 = Matrix4x4.TRS(followPos, lookRotation, Vector3.one);
+        Matrix4x4 combined = m1 * m2;
+
+        // Get pos and rotation from a matrix
+        Vector3 position = combined.GetColumn(3);
+
+        Quaternion rotation = Quaternion.LookRotation
+        (
+            combined.GetColumn(2),
+            combined.GetColumn(1)
+        );
+
+        Quaternion wantedRotation = rotation;
+        Quaternion currentRotation = transform.rotation;
+
+        Vector3 wantedPos = position;
+        Vector3 currentPos = transform.position;
+
+        currentRotation = Quaternion.Lerp
+        (
+            currentRotation,
+            wantedRotation,
+            rotationDamping * Time.deltaTime
+        );
+
+        currentPos = Vector3.Lerp
+        (
+            currentPos,
+            wantedPos,
+            heightDamping * Time.deltaTime
+        );
+
+        transform.localRotation = currentRotation;
+        transform.localPosition = currentPos;
     }
 }
