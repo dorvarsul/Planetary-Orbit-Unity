fb2f938eb90ff4b75877fe225b41b20b12cf5fbd
Dor Varsulker <86845455+dorvarsul@users.noreply.github.com>
2022-10-21 16:04:10 +0300

    Initial commit - planet generation
    Start of procedural planet generation, currently this only created a sphere mesh with vertices evenly distributed between the sphere.

---
+++ Scripts/PlanetGeneration/Planet.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [Range(2,256)]
+    public int _resolution = 10;
+    
+    [SerializeField, HideInInspector]
+    MeshFilter[] _meshFilters;
+    TerrainFace[] _terrainFaces;
+
+    private void OnValidate()
+    {
+        Initialize();
+        GenerateMesh();
+    }
+
+    // Make 6 sides and initialize a mesh for each face
+    void Initialize()
+    {
+        if (_meshFilters == null || _meshFilters.Length == 0)
+        {
+            _meshFilters = new MeshFilter[6];
+        }
+        _terrainFaces = new TerrainFace[6];
+        Vector3[] directions = {Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back};
+
+        for (int i = 0; i < 6; i++)
+        {
+            if (_meshFilters[i] == null)
+            {
+                GameObject meshObj = new GameObject("mesh");
+                meshObj.transform.parent = transform;
+
+                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
+                _meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+                _meshFilters[i].sharedMesh = new Mesh();
+            }
+
+            _terrainFaces[i] = new TerrainFace(_meshFilters[i].sharedMesh, _resolution, directions[i]);
+        }
+    }
+
+    void GenerateMesh()
+    {
+        foreach (TerrainFace face in _terrainFaces)
+        {
+            face.ConstructMesh();
+        }
+    }
+}
